bea47d1ac82e735d511e338c2c67c5b3ce3ff30b
Arthur Chiao <chiaoarthur@gmail.com>
2016-11-17 19:57:39 -0800

    Working options page

--- build/src/options_page/options.js
+++ build/src/options_page/options.js
@@ -29,122 +29,53 @@ function restore_options() {
 }
 // document.addEventListener('DOMContentLoaded', restore_options);
 
-function start(tab) {
-    chrome.windows.getCurrent(getWindows);
-}
-
-function getWindows(win) {
-    targetWindow = win;
-    chrome.tabs.getAllInWindow(targetWindow.id, getTabs);
-}
-
-function getTabs(tabs) {
-    tabCount = tabs.length;
-    // We require all the tab information to be populated.
-    chrome.windows.getAll({"populate": true}, listTabs);
-}
-
-function listTabs(windows) {
-    var test_table = document.getElementById("test_table");
-
-    for (var i = 0; i < windows.length; i++) {
-        var table = document.createElement("table");
-        table.border = "1";
-
-        for (var j = 0; j < windows[i].tabs.length; j++) {
-            var tab = windows[i].tabs[j];
-
-            var a = document.createElement('a');
-            a.href = tab.url;
-            a.appendChild(document.createTextNode(tab.title));
-            a.setAttribute("title", tab.url);
-            a.addEventListener('click', onAnchorClick);
-
-            // Inserts created elements into the table in the HTML page
-            var row = test_table.insertRow(-1);
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            cell1.innerHTML = String(j + 1) + ".";
-            cell2.appendChild(a);
-            console.log(tab.url);
-        }
-
-        test_table.appendChild(table);
-    }
-}
-
-function recent_callback(callback) {
-    chrome.sessions.getRecentlyClosed(function (sessions) {
-        callback(sessions);
-    });
-}
-
-function create_recent_table(sessions) {
-    var recent_tabs_table = document.getElementById("recent_tabs_table");
-
-    sessions.forEach(function (session, i) {
-        var a = document.createElement('a');
-        if (session.window) {
-            session.window.tabs.forEach(function (tab) {
-                a.href = tab.url;
-                a.appendChild(document.createTextNode(tab.title));
-                a.setAttribute("title", tab.url);
-                a.addEventListener('click', onAnchorClick);
-                console.log(a);
-            });
-        } else {
-            a.href = session.tab.url;
-            a.appendChild(document.createTextNode(session.tab.title));
-            a.setAttribute("title", session.tab.title);
-            a.addEventListener('click', onAnchorClick);
-        }
-        var row = recent_tabs_table.insertRow(i);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        cell1.innerHTML = String(i + 1) + ".";
-        cell2.appendChild(a);
-    });
-}
-
 function create_saved_table() {
     var saved_tabs_table = document.getElementById("saved_tabs_table");
-    console.log("here");
-    chrome.storage.sync.get("saved_tabs", function (items) {
-        console.log("lookhere");
+
+    chrome.storage.local.get("saved_tabs", function (items) {
+        console.log("saved table");
         console.log(items.saved_tabs);
         for (var i = 0; i < items.saved_tabs.length; i++) {
 
             // Creates table elements along with tooltips
             var a = document.createElement('a');
-            a.href = items.saved_tabs[i].url;
-            a.appendChild(document.createTextNode(items.saved_tabs[i].title));
-            a.setAttribute("title", items.saved_tabs[i].title);
-            a.addEventListener('click', onAnchorClick);
+            a.href = items.saved_tabs[i];
+            a.appendChild(document.createTextNode(items.saved_tabs[i]));
+            a.setAttribute("title", items.saved_tabs[i]);
+            // a.addEventListener('click', onAnchorClick);
 
-            // EDITS
-            //button to remove a tab from a save_tabs call
-            /*var btn = document.createElement('p');
-             btn.addEventListener("click",removeSaveTab);
-             var btnText = document.createTextNode('X');
-             btn.appendChild(btnText);*/
+            // var favicon = document.createElement('img');
+            // favicon.rel = 'shortcut icon';
+            // console.log(items);
+            // favicon.src = items.saved_tabs[i].favIconUrl;
+            // favicon.type = 'image/x-icon';
+            // favicon.width = "20";
+            //
+            //button to exclude a tab from a save_tabs call
+            var btn = document.createElement('BUTTON');
+            // btn.addEventListener("click", removeSaveTab);
+            btn.className = "btn btn-primary btn-sm";
+            var btnText = document.createTextNode('Remove');
+            btn.appendChild(btnText);
 
             // Inserts created elements into the table in the HTML page
             var row = saved_tabs_table.insertRow(i);
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
-            // var cell3 = row.insertCell(2);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
             cell1.innerHTML = String(i + 1) + ".";
             cell2.appendChild(a);
-            /*cell3.appendChild(btn);*/
+            // cell3.appendChild(favicon);
+            cell4.appendChild(btn);
         }
     });
 };
 
 function init() {
-    console.log("options");
+    console.log("Creating Saved Table From Options");
     create_saved_table();
-    // restore_options();
-    recent_callback(create_recent_table);
+
     document.getElementById('history').addEventListener('click', function() {
         chrome.tabs.update({ url: 'chrome://chrome/history' });
     });
@@ -157,12 +88,9 @@ function init() {
     document.getElementById('about').addEventListener('click', function() {
         chrome.tabs.update({ url: 'chrome://chrome/help' });
     });
-    document.getElementById('save').addEventListener('click',
-        save_options);
 }
 
 console.log("OPTIONS");
-// document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init);
 // document.getElementById('save').addEventListener('click',
 //     save_options);
-recent_callback(create_recent_table);
